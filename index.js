8700c6e93240ec1c02d1239866663848ef642ac8
Greg <aquamirror@gmail.com>
2021-11-27 21:31:57 -0800

    setting up main folders and JS files

---
+++ index.js
@@ -0,0 +1,6 @@
+const generateHTML = require('./src/generateHTML');
+
+const Manager = require('./lib/Manager');
+const Enginner = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+
